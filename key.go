7b4f0c8b84ccd5d7e2bb63b2e1e0b0efa7aa2199
Yinyin L <yinyinl@gmail.com>
2019-08-13 01:16:36 +0800

    split key generation and packaging

--- key.go
+++ key.go
@@ -6,28 +6,33 @@ import (
 	"errors"
 )
 
-// KeySize is the required size in bytes for encryption key
+// KeySize is the required size in bytes for encryption key.
 const KeySize = 32
 
-// ErrKeySizeInsufficient indicates the given key does not have sufficient bytes
+// ErrKeySizeInsufficient indicates the given key does not have sufficient bytes.
 var ErrKeySizeInsufficient = errors.New("insufficient bytes for encryption key")
 
-// GenerateKeyString generates a key and pack into string with BASE-64 for encrypting token
-func GenerateKeyString() (keyText string, err error) {
-	b := make([]byte, KeySize)
-	if _, err = rand.Read(b); nil != err {
-		return "", err
+// GenerateKey generate a key with required key size.
+func GenerateKey() (keyBinary []byte, err error) {
+	keyBinary = make([]byte, KeySize)
+	if _, err = rand.Read(keyBinary); nil != err {
+		return nil, err
 	}
-	keyText = base64.StdEncoding.EncodeToString(b)
-	return keyText, nil
+	return
 }
 
-func unpackKeyString(keyText string) (keyBinary []byte, err error) {
-	if keyBinary, err = base64.StdEncoding.DecodeString(keyText); nil != err {
+// PackKeyToString encode given key binary into string with BASE-64 encoding.
+func PackKeyToString(keyBinary []byte) (keyText string) {
+	keyText = base64.RawStdEncoding.EncodeToString(keyBinary)
+	return
+}
+
+// UnpackKeyFromString decode given key binary from string in BASE-64 encoding.
+func UnpackKeyFromString(keyText string) (keyBinary []byte, err error) {
+	if keyBinary, err = base64.RawStdEncoding.DecodeString(keyText); nil != err {
 		return
 	}
-	keyLen := len(keyBinary)
-	if keyLen == KeySize {
+	if keyLen := len(keyBinary); keyLen == KeySize {
 		return
 	} else if keyLen > KeySize {
 		return keyBinary[0:KeySize], nil
